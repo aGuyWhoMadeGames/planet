a425058d1bd6cfccf7f60121d37ed6dcf12c6f68
unknown <166567074+aGuyWhoMadeGames@users.noreply.github.com>
2024-06-25 15:26:26 -0500

    Add generator for procedural planets.

---
+++ planet/PlanetGeneratorNoise.gd
@@ -0,0 +1,12 @@
+@tool
+extends PlanetGenerator
+
+class_name PlanetGeneratorNoise
+
+@export var scale = 1000.0
+@export_range(-1.0,1.0) var ocean_value = -1.0
+@export var noise:Noise
+
+func _get_height(v:Vector3) -> float:
+	var h = noise.get_noise_3dv(v*scale)
+	return max(0.0, remap(h,ocean_value,1.0,0.0,1.0))
