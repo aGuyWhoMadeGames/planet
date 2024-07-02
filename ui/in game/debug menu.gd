186e89949694ae58242d1ca5857562bf5dd458dd
unknown <166567074+aGuyWhoMadeGames@users.noreply.github.com>
2024-07-02 13:42:48 -0500

    Update debug menu.

--- ui/in game/debug menu.gd
+++ ui/in game/debug menu.gd
@@ -3,16 +3,20 @@ extends Label
 var wireframe = false
 
 var template = """
+{cpu}
+{gpu}
 Fps: {fps}
-Vertices: {verts}
+Triangles: {tris}
 Resolution: {xres}x{yres}
 Memory:{mem}Mb
 """
 
 func _process(_delta):
 	text = template.format({
+		"cpu":OS.get_processor_name(),
+		"gpu":RenderingServer.get_video_adapter_name(),
 		"fps":Engine.get_frames_per_second(),
-		"verts":RenderingServer.get_rendering_info(RenderingServer.RENDERING_INFO_TOTAL_PRIMITIVES_IN_FRAME),
+		"tris":RenderingServer.get_rendering_info(RenderingServer.RENDERING_INFO_TOTAL_PRIMITIVES_IN_FRAME),
 		"xres":get_viewport().size.x,
 		"yres":get_viewport().size.y,
 		"mem":round((OS.get_static_memory_usage())*0.000001)
